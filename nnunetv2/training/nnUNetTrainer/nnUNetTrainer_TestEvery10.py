e74188b91d22b96ce06f01cc3d988d84166e8d36
entity079 <153630586+entity079@users.noreply.github.com>
2026-01-09 05:02:38 +0530

    Add nnUNetTrainer_TestEvery10 for periodic testing

---
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer_TestEvery10.py
@@ -0,0 +1,37 @@
+import subprocess
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+
+class nnUNetTrainer_TestEvery10(nnUNetTrainer):
+
+    def on_epoch_end(self):
+        super().on_epoch_end()
+
+        if (self.current_epoch + 1) % 10 == 0:
+            self.logger.log(
+                f"Running TEST inference at epoch {self.current_epoch + 1}"
+            )
+
+            self.run_test_inference()
+
+    def run_test_inference(self):
+        dataset_name = self.dataset_name
+        configuration = self.configuration
+        fold = self.fold
+        trainer = self.__class__.__name__
+
+        output_dir = (
+            f"{self.output_folder}/test_epoch_{self.current_epoch + 1}"
+        )
+
+        cmd = [
+            "nnUNetv2_predict",
+            "-i", f"$nnUNet_raw/Dataset{dataset_name}/imagesTs",
+            "-o", output_dir,
+            "-d", dataset_name,
+            "-c", configuration,
+            "-f", str(fold),
+            "-tr", trainer,
+            "--disable_tta"
+        ]
+
+        subprocess.run(" ".join(cmd), shell=True, check=True)
